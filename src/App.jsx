ab36f55f921b941687eeefc8e7e5c8bc3de764cb
geekyhimanshu2309 <himanshu112760@gmail.com>
2024-01-21 00:32:51 +0530

    project completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(10)
+  const [color, setColor] = useState("olive")
 
   return (
-      <div className='bg-white'>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
+    <div className="flex h-screen w-full items-center justify-center"
+    style={{backgroundColor: color}}>
+      <div className="fixed flex-col justify-between outline-none rounded-3xl border-2 bottom-12 bg-white p-1">
+        <button
+          className="bg-red-600 px-2 outline-none rounded-3xl border-2"
+          onClick={() => setColor("red")}
+        >
+          Red
+        </button>
+        <button
+          className="bg-blue-600 rounded-3xl px-2 outline-none border-2"
+          onClick={() => setColor("blue")}
+        >
+          BLue
+        </button>
+        <button
+          className="bg-violet-600 rounded-3xl px-2 outline-none border-2"
+          onClick={() => setColor("violet")}
+        >
+          violet
+        </button>
       </div>
-  )
+    </div>
+  );
 }
 
 export default App;
